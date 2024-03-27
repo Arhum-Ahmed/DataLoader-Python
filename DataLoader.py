491a0a8483959e72d145762a358c9c21fb266662
Arhum-Ahmed <arhumahmed99@gmail.com>
2024-03-27 21:23:44 +0500

    Updated DataLoader file

--- DataLoader.py
+++ DataLoader.py
@@ -13,24 +13,6 @@ import cv2
 import numpy as np
 import random
 
-def load_images(data_dir):
-    """
-        This function loads the dataset.
-        data_dir => Directory of the folder to extract data from
-        data_set => Returns a list with tuple elements (img_array, label)
-    """
-    data_set = []
-    list_classes = os.listdir(data_dir)
-    for i in list_classes:
-        sub_dir   = os.path.join(data_dir, i)
-        sub_class = os.listdir(sub_dir)
-        for img in sub_class:
-            img_path = os.path.join(sub_dir, img)
-            img_array = np.array(cv2.imread(img_path))
-            data_set.append((img_array, i))
-    return data_set
-
-
 class DataLoader():
     def __init__(self, dataset, batch_size, shuffle=True):
         self.batch_size = batch_size
@@ -54,10 +36,32 @@ class DataLoader():
         return batch_out
 
 
+def load_images(data_dir):
+    """
+        This function loads the dataset.
+        data_dir => Directory of the folder to extract data from
+        data_set => Returns a list with tuple elements (img_array, label)
+    """
+    data_set = []
+    list_classes = os.listdir(data_dir)
+    for i in list_classes:
+        sub_dir   = os.path.join(data_dir, i)
+        sub_class = os.listdir(sub_dir)
+        for img in sub_class:
+            img_path = os.path.join(sub_dir, img)
+            img_array = np.array(cv2.imread(img_path))
+            data_set.append((img_array, i))
+    return data_set
 
 
-data_set_train = load_images('train')
+data_set_train      = load_images('train')
+data_set_test       = load_images('test')
+data_set_validation = load_images('validation')
 
-train_data = DataLoader(data_set_train, batch_size=100, shuffle=True)
+train_data      = DataLoader(data_set_train, batch_size=3, shuffle=False)
+test_data       = DataLoader(data_set_test, batch_size=3, shuffle=False)
+validation_data = DataLoader(data_set_validation, batch_size=3, shuffle=False)
 
 print(next(train_data))
+print(next(test_data))
+print(next(validation_data))
